6e8d0305c81828fec387250cfd5a88487c5b845b
Grandpa EJ <fakeif352@gmail.com>
2024-10-24 20:29:40 -0700

    Update __init__.py

--- gpColor/__init__.py
+++ gpColor/__init__.py
@@ -1,5 +1,5 @@
 # Import the main functions and classes from the color module
-from .color import Color, colorize, ansi_color, rgb_color, hex_to_rgb
+from .color import colorize
 
 # Define the public interface of the package
-__all__ = ['Color', 'colorize', 'ansi_color', 'rgb_color', 'hex_to_rgb']
+__all__ = ['colorize']
